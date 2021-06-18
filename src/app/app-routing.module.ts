15ca9c4edc06acd238365bcc2e9961f1629c4a77
JORGEBOLAÑOS\Jorge <jorge.b16@hotmail.com>
2021-06-18 18:54:42 -0500

    Se generan paginas para citas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'list', loadChildren: () => import('./pages/citas/list/list.module').then(m => m.ListModule) }, { path: 'new', loadChildren: () => import('./pages/citas/new/new.module').then(m => m.NewModule) }, { path: 'details', loadChildren: () => import('./pages/citas/details/details.module').then(m => m.DetailsModule) }, { path: 'edit', loadChildren: () => import('./pages/citas/edit/edit.module').then(m => m.EditModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
