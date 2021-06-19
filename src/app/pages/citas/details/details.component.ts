72fcfe8a6db97a142dcf6a0444a4ca1f026692c2
JORGEBOLAÑOS\Jorge <jorge.b16@hotmail.com>
2021-06-18 19:43:40 -0500

    Se crea listado de citas y detalle

--- src/app/pages/citas/details/details.component.ts
+++ src/app/pages/citas/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  editar(): void {
+    this.router.navigate(['edit']);
+  }
+
+  async eliminar(): Promise<void> {
+    try {
+      alert('Deleted');
+      this.volver();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  volver(): void {
+    this.router.navigate(['list']);
   }
 
 }
--- src/app/pages/citas/list/list.component.ts
+++ src/app/pages/citas/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  navigationExtras: NavigationExtras = {
+    state: {
+      value: null
+    }
+  };
+
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
   }
 
+  editar(item:any):void{
+    this.router.navigate(['edit'])
+  }
+  ver(item:any):void{
+    this.router.navigate(['details'])
+  }
+  eliminar(item:any):void{
+    alert('Se ha borrado tu cita')
+  }
+  
 }
